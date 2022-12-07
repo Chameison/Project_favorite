45e8bd61ea3836c96381458ce2f12ad1774cc95a
Chameison <chameison.acre@gmail.com>
2022-12-06 20:11:31 -0500

    Projeto finalizado e funcionando

--- Project_favorite/api/index.js
+++ Project_favorite/api/index.js
@@ -3,17 +3,36 @@ const http = require('http')
 const URL = require('url')
 const fs = require('fs')
 const path = require('path')
+const querystring = require('node:querystring');
+
 // importando docuemtnos do urls.json
 const data = require('./urls.json')
 
+//funcao de escrever
+function writeFile(cb) {
+    fs.writeFile(
+        path.join(__dirname, 'urls.json'),
+        JSON.stringify(data, null, 2),
+        err => {
+            if (err) throw err
+            cb('Operação realizada com sucesso')
+        }
+    )
+}
 // colocando o servidor para rodar na porta 3000
 http.createServer((req, res) => {
+
     const { name, url, del } = URL.parse(req.url, true).query
-    if (!name || !url) 
-        return res.end('show')
-    if (del)
-        return res.end('delete')
-    
-    return res.end('delete')
+    if (!name || !url) {
+        return res.end(JSON.stringify(data))
+    }
+    if (del=="del"){
+        data.urls = data.urls.filter(item => item.url != url)
+        return writeFile(message => res.end(message))
+    }
+
+    data.urls.push({name, url}) //funcao push
+    return writeFile(message => res.end(message))
     // res.end(JSON.stringify(data))
 }).listen(3500, () => console.log("API is Running..."))
+
