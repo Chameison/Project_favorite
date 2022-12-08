c1b883f01210d072ac7947724623c201b3669cb8
Chameison Araújo <chameison.acre@gmail.com>
2022-12-08 16:21:22 -0300

    enviando com a função de adicionar funcionando

--- api/index.js
+++ api/index.js
@@ -9,20 +9,26 @@ const querystring = require('node:querystring');
 const data = require('./urls.json')
 
 //funcao de escrever
-function writeFile(cb) {
-    fs.writeFile(
-        path.join(__dirname, 'urls.json'),
-        JSON.stringify(data, null, 2),
-        err => {
-            if (err) throw err
-            cb('Operação realizada com sucesso')
-        }
-    )
-}
+
 // colocando o servidor para rodar na porta 3000
 http.createServer((req, res) => {
 
     const { name, url, del } = URL.parse(req.url, true).query
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*'
+    })
+
+
+    function writeFile(cb) {
+        fs.writeFile(
+            path.join(__dirname, 'urls.json'),
+            JSON.stringify(data, null, 2),
+            err => {
+                if (err) throw err
+                cb('Operação realizada com sucesso')
+            }
+        )
+    }
     if (!name || !url) {
         return res.end(JSON.stringify(data))
     }
@@ -34,5 +40,5 @@ http.createServer((req, res) => {
     data.urls.push({name, url}) //funcao push
     return writeFile(message => res.end(message))
     // res.end(JSON.stringify(data))
-}).listen(3500, () => console.log("API is Running..."))
+}).listen(3000, () => console.log("API is Running..."))
 
